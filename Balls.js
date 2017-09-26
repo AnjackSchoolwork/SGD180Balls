cb7bbb07116c16fc272aee53b416923391c8a434
Anjack <gary.reif@gmail.com>
2017-09-26 12:55:18 -0400

    Completed

--- Balls.js
+++ Balls.js
@@ -1,1 +1,45 @@
-// JavaScript source code
+
+function setup() {
+	scene = new Scene()
+	ballList = generateBalls(5)
+
+	scene.start()
+}
+
+function update() {
+	scene.clear()
+
+	for (var index in ballList) {
+		ballList[index].update()
+	}
+}
+
+function generateBalls(num) {
+	var tempArray = []
+	for (var interval = 0; interval < num; interval++) {
+		var tempVector = getRandomVector2d()
+		var tempDimension = getRandomInt(10, 100)
+		var tempSprite = new Sprite(scene, "img/redBall.png", tempDimension, tempDimension)
+		tempSprite.setPosition(400, 300)
+		tempSprite.setMoveAngle(tempVector.angle)
+		tempSprite.setSpeed(tempVector.magnitude)
+		tempArray.push(tempSprite)
+	}
+	return tempArray
+}
+
+function getRandomVector2d() {
+	var angle = getRandomInt(0, 360)
+	var magnitude = getRandomInt(1, 10)
+	return {
+		"angle": angle,
+		"magnitude": magnitude
+	}
+}
+
+function getRandomInt(min, max) {
+	min = Math.floor(min)
+	max = Math.ceil(max)
+
+	return Math.floor(Math.random() * (max - min)) + min
+}
